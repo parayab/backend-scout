a41f9cc5ab6ea4205ad7582a940b01c4438baedc
Tomás Freudenberg <tommyefr@gmail.com>
2019-05-12 19:04:59 -0400

    improvement: make function to create sections
    The idea is to use the function somewhere else

---
+++ routes/sectionFunctions.js
@@ -0,0 +1,13 @@
+module.exports = {
+  create: async function(ctx, requestBody) {
+    // requestBody = {name: string, typeId: int, groupId: int}
+    const newSection = ctx.orm.section.build(requestBody);
+    try {
+      await newSection.save({ fields: ["name", "typeId", "groupId"] });
+      const sections = await ctx.orm.section.findAll();
+      return { sections };
+    } catch (validationError) {
+      return { errors: validationError.errors };
+    }
+  }
+};
