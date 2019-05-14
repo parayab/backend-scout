2b217e547b55af1a25b1a7c5cfc63a5496c9b906
Tomás Freudenberg <tommyefr@gmail.com>
2019-05-14 02:57:14 -0400

    improvement: change returned sections for frontend

--- routes/sections/sectionFunctions.js
+++ routes/sections/sectionFunctions.js
@@ -4,7 +4,9 @@ module.exports = {
     const newSection = ctx.orm.section.build(requestBody);
     try {
       await newSection.save({ fields: ["name", "typeId", "groupId"] });
-      const sections = await ctx.orm.section.findAll();
+      const sections = await ctx.orm.section.findAll({
+        where: { groupId: ctx.params.group_id }
+      });
       return { sections };
     } catch (validationError) {
       return { errors: validationError.errors };
