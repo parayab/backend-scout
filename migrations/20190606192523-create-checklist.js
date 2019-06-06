a113cc0db7378d4d23bfb8513e192f0491858dbf
wayoalamos <malamos2@uc.cl>
2019-06-06 16:07:25 -0400

    add association in the model, add groupEventId

--- migrations/20190606192523-create-checklist.js
+++ migrations/20190606192523-create-checklist.js
@@ -7,6 +7,9 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      groupEventId: {
+        type: Sequelize.INTEGER
+      },
       name: {
         type: Sequelize.STRING
       },
--- models/checklist.js
+++ models/checklist.js
@@ -2,10 +2,36 @@ module.exports = (sequelize, DataTypes) => {
   const checklist = sequelize.define(
     "checklist",
     {
-      name: DataTypes.STRING
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Name can't be empty"
+          }
+        }
+      },
+      groupEventId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "GroupEventId can't be empty!!"
+          }
+        }
+      }
     },
     {}
   );
-  checklist.associate = function() {};
+  checklist.associate = models => {
+    checklist.belongsTo(models.groupEvent, {
+      // Will add groupEventId to userJoinGroupEvent model
+      foreignKey: "groupEventId",
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+      as: "groupEvent"
+      // enables groupEvent.getParticipants()
+    });
+  };
   return checklist;
 };
