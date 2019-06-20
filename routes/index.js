9a4c2d05910dff9426a3f0f7f0e88f43bd4f52a3
wayoalamos <malamos2@uc.cl>
2019-06-20 13:21:52 -0400

    add sesion to the routes index

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,14 @@ const book = require("./book");
 const group = require("./group");
 // const section = require("./section");
 const user = require("./users/sectionUsers");
+const session = require("./session");
 
 const router = new Router();
 
 router.use("/", home.routes());
 router.use("/books", book.routes());
 router.use("/groups", group.routes());
+router.use("/session", session.routes());
 // router.use("/sections", section.routes());
 // router.use("/users", user.routes());
 
